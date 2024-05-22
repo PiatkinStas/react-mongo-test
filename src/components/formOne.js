e6f9e6030a5406f371d79e2baca8a57984a6a36f
Piatkin Stas <piatkinstas@gmail.com>
2024-05-22 20:08:58 +0300

    Удалил стандартные файлы, добавил папку компонентов и форму

---
+++ src/components/formOne.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+
+const FormComponent = () => {
+  const [value, setValue] = useState(''); // value - строка input
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/firstform', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ value: value }),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="formOne">Сообщения формы №1:</label>
+        <input
+          type="text"
+          id="formOne"
+          value={value} // value из useState
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Отправить</button>
+    </form>
+  );
+};
+
+export default FormComponent;
